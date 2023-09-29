849caebfa309249ac441f347914c320ade3f4f74
aukkawut <aammartayakun@wpi.edu>
2023-09-28 22:20:47 -0400

    make it searchable

--- app/utils.py
+++ app/utils.py
@@ -2,16 +2,16 @@ from scholarly import scholarly
 
 def search_google_scholar(query):
     search_query = scholarly.search_pubs(query)
-    papers = [next(search_query) for _ in range(10)]
-    
+    papers = [next(search_query) for _ in range(20)]
     professors = []
     for paper in papers:
-        authors = paper.bib.get('author', '').split(', ')
-        if authors:
+        authors = paper['author_id']
+        if len(authors)!=0:
             last_author = authors[-1]
-            affiliations = paper.bib.get('eprint', '').split('@')
-            uni_name = affiliations[1] if len(affiliations) > 1 else ""
-            if ".edu" in uni_name:
-                professors.append(last_author)
-
+            if last_author != '':
+                #search that author based on the author id
+                last_author_information = scholarly.search_author_id(last_author)
+                #print(last_author_information.keys())
+                if '.edu' in last_author_information['email_domain'] and last_author_information['name'] not in professors:
+                    professors.append(last_author_information['name'])
     return professors
